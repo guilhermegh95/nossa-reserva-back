fdcb1a93042d0cbf27804067ba8ad6284c1e51b1
Guilherme Henrique de Souza <47610276+guilhermegh95@users.noreply.github.com>
2022-03-18 04:27:57 -0300

    First upload

---
+++ nosssaReservaBackend/src/main/java/com/nossareserva/NossareservaApplication.java
@@ -0,0 +1,13 @@
+package com.nossareserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossareservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossareservaApplication.class, args);
+	}
+
+}
