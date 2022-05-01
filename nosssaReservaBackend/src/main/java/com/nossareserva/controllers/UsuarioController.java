21d9bf709144928d1bd37955c42b6dc09db912a5
Guilherme <guilherme.gh95@gmail.com>
2022-05-01 01:33:13 -0300

    ADD PUT, POST AND DELETE METHODS TO USUARIOCONTROLLER

--- nosssaReservaBackend/src/main/java/com/nossareserva/controllers/UsuarioController.java
+++ nosssaReservaBackend/src/main/java/com/nossareserva/controllers/UsuarioController.java
@@ -2,11 +2,11 @@ package com.nossareserva.controllers;
 import com.nossareserva.UsuarioModel;
 import com.nossareserva.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -39,5 +39,34 @@ public class UsuarioController {
         }
     }
 
+    @PostMapping("/usuario")
+    public ResponseEntity<UsuarioModel> saveUsuario(@RequestBody @Validated UsuarioModel usuario) {
+        return new ResponseEntity<UsuarioModel>(usuarioRepository.save(usuario), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/usuario/{id}")
+    public ResponseEntity<?> deleteUsuario(@PathVariable(value = "id") long id) {
+        Optional<UsuarioModel> usuarioUnitario = usuarioRepository.findById(id);
+        if(!usuarioUnitario.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else {
+            usuarioRepository.delete(usuarioUnitario.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+
+    @PutMapping("produtos/{id}")
+    public ResponseEntity<UsuarioModel> updateUsuario(@PathVariable(value = "id") long id,
+                                                      @RequestBody @Validated UsuarioModel usuario) {
+        Optional<UsuarioModel> usuarioUnitario = usuarioRepository.findById(id);
+        if(usuarioUnitario.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else {
+            usuario.setId(usuarioUnitario.get().getId());
+            return new ResponseEntity<UsuarioModel>(usuarioRepository.save(usuario), HttpStatus.OK);
+        }
+    }
 
 }
